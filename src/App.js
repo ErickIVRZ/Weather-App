3b647fed193dbbedcd325bd70c78ab08b8ee2972
Erick Valdez <erickvrz.92@gmail.com>
2022-04-05 17:55:17 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import logo from './logo.svg';
+
 import './App.css';
 import SearchWeather from './component/SearchWeather';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import React, {useState} from 'react';
-
-
-
 
 
 
